51c8de70c7cc8725297e0ef3d9a88dd24d154a59
cf1887 <chrisfernholz@web.de>
2023-04-24 15:20:03 +0200

    02_Voter: Vue-Root-Instanz erstellen und an Element binden

---
+++ 02_Voter/app.js
@@ -0,0 +1,6 @@
+const app = Vue.createApp({
+    // Optionen (auch globale Variablen)
+});
+
+// Liefer die Instanz zur Root-Component zurück, das sogenannte View-Model (vm)
+const vm = app.mount('#app');
