7acd68d28ebe0378841d7deac79415d25bba6618
cf1887 <chrisfernholz@web.de>
2023-04-24 15:32:57 +0200

    02_Voter: Das 'data'-Attribut

--- 02_Voter/app.js
+++ 02_Voter/app.js
@@ -1,5 +1,10 @@
 const app = Vue.createApp({
     // Optionen (auch globale Variablen)
+    data: function() {
+        return {
+            submissions: submissions // aus seed.js 
+        }
+    }
 });
 
 // Liefer die Instanz zur Root-Component zurück, das sogenannte View-Model (vm)
