6c74750f8eaac60c6a717c4d141287d66177e238
cf1887 <chrisfernholz@web.de>
2023-05-01 13:35:06 +0200

    02_Voter: App aufgeräumt

--- 02_Voter/app.js
+++ 02_Voter/app.js
@@ -2,20 +2,16 @@ const app = Vue.createApp({
     // Optionen (auch globale Variablen)
     data: function() {
         return {
-            submissions: submissions, // aus seed.js 
-            totalVotes: 0,
+            submissions: submissions, // aus seed.js
         };
     },
-    /**
-     * Berechnete Werte (bei Änderungen) - Wie Daten verwendet, aber wie Funktionen definiert.
-     * Computed-Values sind besser für die Berechnung von Daten aus Daten als Methoden, denn
-     * sie werden nur bei tatsächlichen Änderungen ausgeführt.
-     * Beispiel: Eine Methode 'totalVotes()' ermittelt die Gesamtmenge aller Votes aller Einträge.
-     * Ändert man nun irgendwann den Titel eines Eintrags, muss der Wert der Votes theoretisch nicht neu berechnet werden.
-     * Ist 'totalVotes()' eine Methode (methods), wird sie bei JEDER Änderung ausgeführt.
-     * Ist 'totalVotes()' aber ein computed-Value (computed), wird sie nur bei tatsächlichen Änderungen ausgeführt.
-     */ 
+    // ComputedProperties
     computed: {
+        totalVotes() {
+            return this.submissions.reduce((totalVotes, submission) => {
+                return totalVotes + submission.votes;
+            }, 0);
+        },
         sortedSubmissions() {
             return this.submissions.sort((a, b) => {
                 return b.votes - a.votes;
@@ -28,30 +24,9 @@ const app = Vue.createApp({
             const submission = this.submissions.find((submission) => submission.id === submissionId);
             submission.votes++;
         },
-        logConsole(text) {
-            console.log(text);
-        },
     },
     // Watcher / (Daten-) Beobachter
     watch: {
-        // Beobachte Variable 'submissions' auf Veränderungen
-        submissions: {
-            handler(newvalue, oldValue) {
-                // Überschreibe 'globale' Variable
-                this.totalVotes = this.submissions.reduce((totalVotes, submission) => {
-                    return totalVotes + submission.votes;
-                }, 0);
-            },
-            // Beobachte auch Daten INNERHALB des Objekts (oder Arrays)
-            deep: true,
-            // Führe Watcher sofort (also bereits bei der Erstellung des Objekts) aus
-            immediate: true,
-        },
-        // Beobachte Variable 'totalVotes' auf Veränderungen
-        totalVotes(newvalue, oldValue) {
-            console.log(newvalue);
-            console.log(oldValue);
-        }
     }
 });
 
