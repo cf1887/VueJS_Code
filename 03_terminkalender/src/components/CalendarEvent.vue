51c54f90f5402494d94b4718d4312f8f98063487
cf1887 <chrisfernholz@web.de>
2023-05-19 18:42:44 +0200

    03_terminkalender: v-if und v-else für unterschiedliche Templates

--- 03_terminkalender/src/components/CalendarEvent.vue
+++ 03_terminkalender/src/components/CalendarEvent.vue
@@ -1,56 +1,65 @@
 <template>
   <div id="calendar-event">
     <div class="alert text-center" :class="alertColor">
-      <div>
-        <!-- Veröffentlichung der computed Property dieser Klasse für Slot-Befüllung -->
-        <slot name="eventPriority" :priorityDisplayName="priorityDisplayName">
+      <!-- Template für den Fall, dass das CalendarEvent NICHT bearbeitet wird -->
+      <template v-if="!event.edit">
+        <div>
+          <!-- Veröffentlichung der computed Property dieser Klasse für Slot-Befüllung -->
+          <slot name="eventPriority" :priorityDisplayName="priorityDisplayName">
             <!-- Alles, was zwischen dem öffnenden und dem schließenden Slot-tag steht, ist der sogenannte "FallbackContent". -->
             <!-- Ein FallbackContent ist der Inhalt, welcher (default) angezeigt wird, wenn der Slot NICHT von außen befüllt wird. -->
             <strong>{{ priorityDisplayName }}</strong>
+          </slot>
+        </div>
+        <slot :event="event">
+          <div>{{ event.title }}</div>
         </slot>
-      </div>
-      <slot :event="event">
-        <div>{{ event.title }}</div>
-      </slot>
 
-      <div>
-        <i class="fas fa-edit me-2" role="button"></i>
-        <i class="far fa-trash-alt" role="button" @click="deleteEvent()"></i>
-      </div>
+        <div>
+          <i class="fas fa-edit me-2" role="button"></i>
+          <i class="far fa-trash-alt" role="button" @click="deleteEvent()"></i>
+        </div>
+      </template>
+      <template v-else>
+        <p>Moin!</p>
+      </template>
     </div>
   </div>
 </template>
 
 <script>
-import Store from '../store';
+import Store from "../store";
 export default {
   name: "CalendarEvent",
   props: {
     event: {
-        type: Object,
+      type: Object,
     },
     day: {
-        type: Object,
-    }
+      type: Object,
+    },
   },
   computed: {
     priorityDisplayName: function () {
-        switch (this.event.priority) {
-            case 1: return "Tief";
-            case 0: return "Mittel";
-            case -1: return "Hoch";
-        }
-        return "Unbekannte Priority";
+      switch (this.event.priority) {
+        case 1:
+          return "Tief";
+        case 0:
+          return "Mittel";
+        case -1:
+          return "Hoch";
+      }
+      return "Unbekannte Priority";
     },
     alertColor: function () {
-        return "alert-" + this.event.color;
-    }
+      return "alert-" + this.event.color;
+    },
   },
   methods: {
     deleteEvent: function () {
-        Store.mutations.deleteEvent(this.day.id, this.day.title);
+      Store.mutations.deleteEvent(this.day.id, this.day.title);
     },
-  }
+  },
 };
 </script>
 
