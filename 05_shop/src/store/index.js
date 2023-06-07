1877cb50cc349b297acd2595f0f14462d9209031
cf1887 <chrisfernholz@web.de>
2023-06-07 12:56:14 +0200

    05_shop: Vorbereitung des Stores für den Warenkorb

--- 05_shop/src/store/index.js
+++ 05_shop/src/store/index.js
@@ -1,11 +1,13 @@
 import { createStore } from "vuex";
 import authModule from "./modules/auth/index.js";
 import shopModule from "./modules/shop/index.js";
+import cartModule from "./modules/cart/index.js";
 
 const store = createStore({
     modules: {
         auth: authModule,
         shop: shopModule,
+        cart: cartModule,
     }
 });
 
