76b38f1a5e3bf1782491bf7c19ee49c121238b36
cf1887 <chrisfernholz@web.de>
2023-06-08 12:30:18 +0200

    07_listen: DarkMode für Notification funktionsfähig gemacht

--- 07_listen/src/components/Notification.vue
+++ 07_listen/src/components/Notification.vue
@@ -2,7 +2,7 @@
     <div
         v-if="notification.active"
         class="alert alert-dismissible"
-        :class="{ 'alert-light': isDark, 'alert-dark': !isDark }"
+        :class="{ 'alert-light': darkMode, 'alert-dark': !darkMode }"
     >
         {{ notification.message }}
         <button
@@ -15,8 +15,15 @@
 </template>
 
 <script>
+import useDarkMode from "@/hooks/useDarkMode";
 export default {
     name: "Notification",
-    props: ["notification", "isDark", "toggleNotification"],
+    props: ["notification", "toggleNotification"],
+    setup() {
+        const { darkMode } = useDarkMode();
+        return {
+            darkMode,
+        }
+    },
 };
 </script>
