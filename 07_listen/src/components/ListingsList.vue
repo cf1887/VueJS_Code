2ec86ebbdcfecb84961931fa87d2d1a984bf6b0d
cf1887 <chrisfernholz@web.de>
2023-06-08 11:29:17 +0200

    07_listen: Umschreibung der ListingsList.vue von OptionsAPI in CompositionAPI

--- 07_listen/src/components/ListingsList.vue
+++ 07_listen/src/components/ListingsList.vue
@@ -1,47 +1,71 @@
 <template>
-  <div id="listings">
-    <Notification :notification="notification" :isDark="isDark" />
-    <div class="row row-cols-1 row-cols-md-3 g-4">
-      <div class="col" v-for="listing in listings" :key="listing.id">
-        <ListingsListItem :listing="listing" :isDark="isDark" />
-      </div>
+    <div id="listings">
+        <Notification :notification="notification" :isDark="isDark" />
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col" v-for="listing in listings" :key="listing.id">
+                <ListingsListItem :listing="listing" :isDark="isDark" />
+            </div>
+        </div>
+        <button
+            class="btn btn-primary mt-2"
+            @click="resetListings"
+            :disabled="listings.length === 3"
+        >
+            Reset
+        </button>
     </div>
-    <button
-      class="btn btn-primary mt-2"
-      @click="resetListings"
-      :disabled="listings.length === 3"
-    >
-      Reset
-    </button>
-  </div>
 </template>
 
 <script>
-import { mapActions } from "vuex";
+import { ref, onMounted } from "vue";
+import { useStore } from "vuex";
+// import { mapActions } from "vuex";
 import ListingsListItem from "./ListingsListItem";
 import Notification from "./Notification";
 
 export default {
-  name: "ListingsList",
-  props: ["listings", "isDark"],
-  data() {
-    return {
-      notification: null,
-    };
-  },
-  methods: {
-    ...mapActions(["resetListings"]),
-  },
-  components: {
-    ListingsListItem,
-    Notification,
-  },
-  mounted() {
-    this.notification = "Herzlichen Willkommen!";
+    name: "ListingsList",
+    components: {
+        ListingsListItem,
+        Notification,
+    },
+    props: ["listings", "isDark"],
 
-    setTimeout(() => {
-      this.notification = null;
-    }, 3000);
-  },
+    setup() {
+        // Store:
+        const store = useStore();
+        // Data:
+        const notification = ref(null);
+        // Methods:
+        const resetListings = () => store.dispatch("resetListings");
+        // Mounted-Hook:
+        onMounted(() => {
+            notification.value = "Herzlich Willkommen!";
+            setTimeout(() => {
+                notification.value = null
+            }, 3000);
+        })
+        return {
+            notification,
+            resetListings,
+        };
+    },
+
+    // data() {
+    //     return {
+    //         notification: null,
+    //     };
+    // },
+    // methods: {
+    //     ...mapActions(["resetListings"]),
+    // },
+    
+    // mounted() {
+    //     this.notification = "Herzlichen Willkommen!";
+
+    //     setTimeout(() => {
+    //         this.notification = null;
+    //     }, 3000);
+    // },
 };
 </script>
