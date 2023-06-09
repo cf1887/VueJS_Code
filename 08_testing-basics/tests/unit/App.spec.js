57597a53aa3b20c0a6d4a2bf6f93a590f463bd5d
cf1887 <chrisfernholz@web.de>
2023-06-09 11:36:05 +0200

    08_testing-basics: Weitere Tests zur initialen App.vue-Component erstellt

--- 08_testing-basics/tests/unit/App.spec.js
+++ 08_testing-basics/tests/unit/App.spec.js
@@ -7,6 +7,7 @@
 
 import App from '@/App.vue';
 import { expect } from 'chai';
+import { shallowMount } from '@vue/test-utils';
 
 // Testgruppe
 describe('App.vue-Component', () => {
@@ -15,12 +16,50 @@ describe('App.vue-Component', () => {
     //     expect("Hello " + "World!").to.equal("Hello World!");
     // });
 
-    // Erster richtiger Test:
+    /**
+     * Hilfsfunktion zum Mounten der App-Component vom vue/test-utils Plugin.
+     * Wichtig ist hier, dass man die shallowMount()-Funktion nutzt,
+     * da sonst auch alle Kind-Components geladen werden würden und
+     * wenn es bei den Kind-Components zu Fehlern kommt, würden
+     * diese Fehler hier auch auftreten - das wäre zumindest nicht zielführend.
+     */
+    let wrapper;
+
+    /**
+     * Vor jedem Test-Case:
+     * Mounte die App neu (vermeidet ungewollte Seiteneffekte).
+     */
+    beforeEach(() => {
+        wrapper = shallowMount(App);
+    });
+
+    /**
+     * Test für initiale Werte
+     */ 
     it ('Setzt die initialen Daten korrekt.', () => {
         // Hole die initial gesetzten Daten der App-Component
-        const initialData = App.data();
+        // const initialData = App.data();
         // Erwartungen mit Chai formulieren:
-        expect(initialData.item).to.equal('');
-        expect(initialData.items).to.deep.equal([]);
+        // Hinweis: wrapper.vm holt die Vue-Instanz (damit können wir auf Daten prüfen).
+        expect(wrapper.vm.item).to.equal('');
+        expect(wrapper.vm.items).to.deep.equal([]);
+    });
+
+    /**
+     * Suche nach Inhalten im GERENDERTEN DOM (also nicht mehr im Template, sondern bereits im erstellten Html-Code).
+     */
+    it ('Rendert das korrekte Template.', () => {
+        expect(wrapper.html()).to.contain('<th class="fs-2">Einträge</th>');
+        expect(wrapper.html()).to.contain('<input type="text" class="form-control" placeholder="Eintrag hinzufügen...">');
+    });
+
+    /**
+     * Prüfe, ob der Hinzufügen-Button initial disabled ist.
+     */
+    it ('Setzt den Button \'Hinzufügen\' initial auf disabled.', () => {
+        // Am besten mit einer Id danach suchen.
+        // Hier reicht aber die Klasse, da wir eh nur einen (primary) button haben
+        const addItemButton = wrapper.find(".btn.btn-primary");
+        expect(addItemButton.element.disabled).to.be.true;
     });
 });
