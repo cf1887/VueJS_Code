987697f697cc94236f6e21e18c26d74fcd9b0795
cf1887 <chrisfernholz@web.de>
2023-06-09 11:18:48 +0200

    08_testing-basics: Erster Test zur initialen App.vue-Component erstellt

--- 08_testing-basics/tests/unit/App.spec.js
+++ 08_testing-basics/tests/unit/App.spec.js
@@ -5,12 +5,22 @@
  * alle Tests automatisch einmal ausgeführt (siehe '--watch'-Flag).
 */
 
+import App from '@/App.vue';
 import { expect } from 'chai';
 
 // Testgruppe
-describe('App.vue', () => {
+describe('App.vue-Component', () => {
     // Ein Test in der Gruppe
-    it('Dieser Test sollte erfolgreich ausgeführt werden.', () => {
-        expect("Hello " + "World!").to.equal("Hello World!");
+    // it('Dieser Test sollte erfolgreich ausgeführt werden.', () => {
+    //     expect("Hello " + "World!").to.equal("Hello World!");
+    // });
+
+    // Erster richtiger Test:
+    it ('Setzt die initialen Daten korrekt.', () => {
+        // Hole die initial gesetzten Daten der App-Component
+        const initialData = App.data();
+        // Erwartungen mit Chai formulieren:
+        expect(initialData.item).to.equal('');
+        expect(initialData.items).to.deep.equal([]);
     });
 });
